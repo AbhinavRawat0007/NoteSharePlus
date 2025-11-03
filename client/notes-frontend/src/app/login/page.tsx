334ce864df1bef405e8abbe3c2e963ded8e3e8c3
Abhinav Rawat <abhinavrawat680@gmail.com>
2025-11-03 14:47:12 +0530

    Update API URL in login page

--- client/notes-frontend/src/app/login/page.tsx
+++ client/notes-frontend/src/app/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
 
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`,
+        `${process.env.NEXT_PUBLIC_API_URL}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
