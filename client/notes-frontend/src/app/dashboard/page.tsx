9d7c074f122c0a2feb2e255838593f815fe5fac4
Abhinav Rawat <abhinavrawat680@gmail.com>
2025-11-04 11:48:05 +0530

    Update page.tsx

--- client/notes-frontend/src/app/dashboard/page.tsx
+++ client/notes-frontend/src/app/dashboard/page.tsx
@@ -34,7 +34,7 @@ export default function DashboardPage() {
     const fetchData = async () => {
       try {
         // fetch user
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/notes`, {
+        const resUser = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/notes`, {
          headers: { Authorization: `Bearer ${token}` },
            });
 
