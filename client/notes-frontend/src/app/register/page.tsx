fdbd9dbcaf9af5bdfbdbab3881c0a495f6c8d473
Abhinav Rawat <abhinavrawat680@gmail.com>
2025-11-04 10:58:45 +0530

    Update registration API URL to use environment variable

--- client/notes-frontend/src/app/register/page.tsx
+++ client/notes-frontend/src/app/register/page.tsx
@@ -13,7 +13,7 @@ export default function RegisterPage() {
   // ✅ Handle manual registration
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:5001/api/auth/register", {
+    const res =await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, email, password }),
